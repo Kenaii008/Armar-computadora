e2ec6c2e9664397fba51453f8dc95e30519d9e0e
Kenaii008 <al45778@un.edu.mx>
2025-11-27 20:59:37 -0600

    Avance 8

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ function calcular() {
 }
 
 var gabinete = document.getElementsById("gabinete").value;
-if (gabinete== "700")
+
+if (gabinete == "700") {
+    total+= 700;
+}
+
+else if (gabinete == "1200") {
+    total+= 1200;
+}
+else if (gabinete == "500") {
+    total+=550;
+}
+else if (gabinete == "2500") {
+    total+= 2500;
+}
